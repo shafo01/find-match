c60d4f55d05b11926f912cba9b353c20e00eb0cc
Shafeeza <shafeezahussain@yahoo.com>
2019-07-07 19:26:02 -0400

    Included documentation in the app.js

--- js/app.js
+++ js/app.js
@@ -1,30 +1,65 @@
-/*
- * Create a list that holds all of your cards
+/** 
+ * The pictures/values for the cards
+ * Unicode characters were used so the client doesn't have to download the images. 
+ * This improve loading time... less data has to be downloaded.
+ * This is especially helpful for players who are in areas with poor internet connectivity.
+ * The Unicode characters are displayed differently on different devices. This is a positive for uniqueness but a negative for consistency in design and experience.
+ * @global
  */
 
-const flowers = ['&#127801;', '🌼',
+const flowers = ['🌹', '🌼',
     '🌻', '🌺', '🏵', '🌸',
     '🌻', '🌺', '🍀', '🌷',
     '🌷', '🍀', '🌸', '🏵',
-    '🌼', '&#127801;'
+    '🌼', '🌹'
 ];
 
+/**
+ * Keep track on whether the app was started or not
+ * @global
+ */
 let start = false;
+
+/**
+ * Represents the timer.
+ * I learnt how to set timeintervals and update html elements based on change in time from w3schools. 
+ * Some of their codes was referenced for this purpose.
+ * @class
+ */
 
 const timer = (function() {
 
+    /**
+     * Represents the number of seconds since the first card was flipped 
+     * @memberof timer
+     * @property {number}  0  number of seconds
+     */
     let numSeconds = 0;
 
-    /** some taken from w3schools **/
+    /**
+     * After the game has started, for every second, update the web-page to reflect how long since the game has started in seconds.
+     * @memberof timer
+     * @function
+     */
     function updateTime() {
         document.getElementById("seconds").innerHTML = numSeconds;
         numSeconds++;
     }
 
+    /** 
+     * Since numSeconds is treated as a private membe of timer, this function is used to retrieve it whenever it is needed.
+     * @memberof timer
+     * @function
+     */
     function getNumSeconds() {
         return numSeconds;
     }
 
+    /**
+     * Reset the time to 0 seconds
+     * @memberof timer
+     * @function
+     */
     function resetTime() {
         numSeconds = 0;
         document.getElementById("seconds").innerHTML = numSeconds;
@@ -33,7 +68,6 @@ const timer = (function() {
 
 
     return {
-
         updateTime: updateTime,
         getNumSeconds: getNumSeconds,
         resetTime: resetTime
@@ -42,22 +76,36 @@ const timer = (function() {
 
 })();
 
+
+/**
+ * Stores the time controller - used by setTimeInterval - start and stop time
+ * @global
+ */
+
 let time;
 
 
-
-/*
- * Display the cards on the page
+/**
+ * @description Display the cards on the page:
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
 
 
-
+/**
+ * Store functions that can be used to perform some general actions
+ * @class
+ */
 const ultility = (function() {
 
-    // Shuffle function from http://stackoverflow.com/a/2450976
+
+    /**
+     * Shuffle the cards
+     * Shuffle function is from http://stackoverflow.com/a/2450976
+     * @memberof ultility
+     * @function
+     */
     function shuffle(array) {
         var currentIndex = array.length,
             temporaryValue, randomIndex;
@@ -82,7 +130,7 @@ const ultility = (function() {
 
 
 /*
- * set up the event listener for a card. If a card is clicked:
+ * @description set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
@@ -92,35 +140,85 @@ const ultility = (function() {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+/**
+ * Keep track of all the moves that the player has made. 
+ * It also keep track of the incorrect moves. After some incorrect moves the stars will decrement
+ * @class
+ */
 
 const move = (function() {
 
+    /**
+     * Represents the number of moves or how many times a player turned over a card since the game started
+     * @memberof move
+     * @property {number}  0  number of moves
+     */
+
     let moves = 0;
+
+    /**
+     * Represents the number of moves or how many times a player flipped over a non-match pair of cards
+     * @memberof move
+     * @property {number}  0  number of incorrect moves
+     */
+
     let inCorrectMoves = 0;
+
+    /**
+     * Increment the number of moves the player ade
+     * @memberof move
+     * @function
+     */
 
     function addMove() {
         moves++;
     }
 
-
+    /**
+     * Increment the number of times a player flipped over a non-match pair of cards
+     * @memberof move
+     * @function
+     */
 
     function addIncorrectMove() {
         inCorrectMoves++;
     }
 
+    /**
+     * Returns the number of times a player flipped over a non-match pair of cards
+     * @memberof move
+     * @function
+     */
+
     function getIncorrectMoves() {
         return inCorrectMoves;
     }
 
+    /**
+     * Update the UI every time a player makes a move
+     * @memberof move
+     * @function
+     */
     function updateMoves() {
         let movesSpan = document.getElementById('moves');
         movesSpan.innerHTML = moves;
     }
 
+    /**
+     * Returns the number of moves the player made
+     * @memberof move
+     * @function
+     */
+
     function getMoves() {
         return moves;
     }
 
+    /**
+     * Reset the number of moves to zero
+     * @memberof move
+     * @function
+     */
     function resetMoves() {
         moves = 0;
         updateMoves();
@@ -138,9 +236,27 @@ const move = (function() {
 
 })();
 
+
+/**
+ * Keep track of the stars 
+ * @class
+ */
+
 const star = (function() {
 
+    /**
+     * Represents the number of stars the player currently has
+     * @memberof star
+     * @property {number}  3  number of stars
+     */
+
     let stars = 3;
+
+    /**
+     * Check and return how many stars the player actually has based on the number of times he/she has made incorrect pairing of cards
+     * @memberof star
+     * @function
+     */
 
     function getRemainingStars() {
         if (move.getIncorrectMoves() <= 8) {
@@ -152,6 +268,11 @@ const star = (function() {
         }
     }
 
+    /**
+     * If the player now has fewer stars will update the UI to reflect the number of stars the player actually has
+     * @memberof star
+     * @function
+     */
     function updateStars() {
         let tempStars = getRemainingStars();
         if (stars !== tempStars) {
@@ -166,6 +287,12 @@ const star = (function() {
             }
         }
     }
+
+    /**
+     * Reset the stars on the UI to show that the player has all of his/her stars
+     * @memberof star
+     * @function
+     */
 
     function resetStars() {
         let starElements = document.getElementsByClassName('star');
@@ -183,8 +310,21 @@ const star = (function() {
 
 })();
 
+/**
+ * Deals with the wining screen - after the player has successfully completed the game
+ * @class
+ */
 const winning = (function() {
 
+    /**
+     * Create and display the winning screen to the user. 
+     * It fades out the player's playing board and display the winning screen:
+     * Tell the player he has won and display link to play again.
+     * The winning screen display one of the flowers in the games for decoration three times.
+     * The number of moves, stars and seconds the player has at the end of the game. 
+     * @memberof winning
+     * @function
+     */
     function createWinningScreen() {
 
         let board = document.getElementById('board');
@@ -205,11 +345,13 @@ const winning = (function() {
         document.getElementById('winningMat__moves').innerHTML = `${move.getMoves()} moves`;
 
         document.getElementById('winningMat__seconds').innerHTML = `${document.getElementById('seconds').innerHTML} seconds`;
-
-
-
-
     }
+
+    /**
+     * If the player wishes to replay the game the winning screen is hidden from sight so that the player can get access to the playing board. 
+     * @memberof winning
+     * @function
+     */
 
     function resetWin() {
         let board = document.getElementById('board');
@@ -229,11 +371,50 @@ const winning = (function() {
 
 })();
 
+/**
+ * Represents a card and all of its functionalities
+ * @class
+ */
+
 const card = (function() {
+
+    /**
+     * After the player has flipped a pair of cards check to see if the pairs match or not.
+     * If the pair doesn't match then have to flip back both cards. Therefore, flipBackBoth = true else flipBackBoth = false
+     * @memberof card
+     * @property {boolean}  3  
+     */
+
     let flipBackBoth = false;
+
+    /**
+     * In a pair, keep track of the the first card flipped.
+     * @memberof card
+     * @property {number}    
+     */
+
     let firstCardId;
+
+    /**
+     * When a card is fliiped check if it is first move of the pairs.
+     * @memberof card
+     * @property {number}    
+     */
     let firstMove = true;
+
+    /**
+     * Keep track of all the correct matched pairs the player made.
+     * When the player has reached 8 matched pairs the player has won the game.
+     * @memberof card
+     * @property {number} 0
+     */
     let matchedPairs = 0;
+
+    /**
+     * Reset cards to their default setting. Used to replay the game.
+     * @memberof card
+     * @function
+     */
 
     function resetCard() {
         flipBackBoth = false;
@@ -241,6 +422,12 @@ const card = (function() {
         firstMove = true;
         matchedPairs = 0;
     }
+
+    /**
+     * When a card is flipped is used to check if a pair was flipped and whether there was a match or not.
+     * @memberof card
+     * @function
+     */
 
     function compareCards(card) {
         let matched = false;
@@ -283,6 +470,12 @@ const card = (function() {
         return matched;
     }
 
+    /**
+     * Update UI to show card is flipped.
+     * @memberof card
+     * @function
+     */
+
     function flipCard(card) {
         card.classList.remove('final-flip');
         card.classList.remove('is-flipped');
@@ -290,6 +483,12 @@ const card = (function() {
             card.classList.add('is-flipped');
         });
     }
+
+    /**
+     * After the first card of the card is flipped keep the value part on display
+     * @memberof card
+     * @function
+     */
 
     function showCard(card) {
 
@@ -300,6 +499,11 @@ const card = (function() {
         });
     }
 
+    /**
+     * When a card is flipped will initialize checking for pair and also if the player has unveiled pairs.
+     * @memberof card
+     * @function
+     */
     function checkForPair(card) {
 
         //alert(card.getAttribute("matched"));
@@ -343,26 +547,25 @@ const card = (function() {
         } else {
 
             if (flipBackBoth) {
-                // alert("FIRST CARDS " + firstCard.id + " html " + firstCard.innerHTML);
-                // alert("Current CARDS " + card.id + " html " + card.innerHTML);
                 move.addIncorrectMove();
-                // alert(star.getRemainingStars());
                 star.updateStars();
 
                 flipCard(card);
                 flipCard(firstCard);
                 firstCardId = undefined;
-                //can get a new firstCardId
 
             }
 
 
         }
 
-
-
-
     }
+
+    /**
+     * Create the UI for the front of the card or the default/cover side. 
+     * @memberof card
+     * @function
+     */
 
     function createCardFront() {
 
@@ -371,9 +574,13 @@ const card = (function() {
         front.classList.add('card__face--front');
         front.innerHTML = '💐';
         return front;
-
     }
 
+    /**
+     * Create the UI for the back of the card or the side with the actual value.
+     * @memberof card
+     * @function
+     */
     function createCardBack(picture) {
 
         const back = document.createElement("div");
@@ -381,8 +588,13 @@ const card = (function() {
         back.classList.add('card__face--back');
         back.innerHTML = picture;
         return back;
-
     }
+
+    /**
+     * For the UI, create a card with its front and back sides. 
+     * @memberof card
+     * @function
+     */
 
     function createCard(picture, id) {
         const cardHolder = document.createElement("div");
@@ -419,13 +631,29 @@ const card = (function() {
 }());
 
 
+/**
+ * A board with cards.
+ * @class
+ */
+
 const board = (function() {
+
+    /**
+     * Clear the board
+     * @memberof board
+     * @function
+     */
 
     function resetBoard() {
         let boardElement = document.getElementById('board');
         boardElement.innerHTML = "";
     }
 
+    /**
+     * Display a new set of shuffled cards.
+     * @memberof board
+     * @function
+     */
     function prepareBoard() {
         ultility.shuffle(flowers);
         let i = 0;
@@ -445,9 +673,17 @@ const board = (function() {
 }());
 
 
+/**
+ * A controller to initialize and reset the game.
+ * @class
+ */
 
 const init = (function() {
 
+    /**
+     * Initialize the game
+     * @memberof init
+     */
     function play() {
         board.prepareBoard();
         let replayElement = document.getElementById('replay');
@@ -458,12 +694,16 @@ const init = (function() {
 
     }
 
+    /**
+     * Replay the game
+     * @memberof init
+     */
+
     function replay() {
         //clear timer
         clearTimeout(time);
         //reset time
         timer.resetTime();
-        // time = setInterval(timer.updateTime, 1000);
         //clear out all of the cards and hopefully their events
         board.resetBoard();
         start = false;
@@ -472,7 +712,6 @@ const init = (function() {
         card.resetCard();
         board.prepareBoard();
         winning.resetWin();
-
     }
 
     return {
@@ -484,5 +723,5 @@ const init = (function() {
 })();
 
 
-
+/** The game starts */
 init.play();
